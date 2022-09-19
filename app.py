f03fce10046f2c2ee2f9d045c9383504b9269991
Thorjuri <112181770+Thorjuri@users.noreply.github.com>
2022-09-19 16:49:05 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,58 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+
 app = Flask(__name__)
 
-# from pymongo import MongoClient
-# client = MongoClient('mongodb+srv://test:sparta@cluster0.zslujo2.mongodb.net/Cluster0?retryWrites=true&w=majority')
-# db = client.dbsparta
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://spart:spart@cluster0.hvvgh.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta_plus_week4
+
+# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
+# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
+SECRET_KEY = 'SPARTA'
+
+# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
+import jwt
+
+# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
+import datetime
+
+# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
+# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
+import hashlib
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('login.html')
 
-@app.route('/login')
-def login():
-   return render_template('index.html')
 
-@app.route('/join')
-def join():
-   return render_template('index.html')
+@app.route('/index')
+def main():
+    return render_template('index.html')
 
-@app.route('/userInput')
-def user_input():
-   return render_template('index.html')
 
-def bmi_calculate(bmiCalculate):
-   return render_template('index.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+# ------------ API 구현 ------------------------
+
+# 1. 회원가입: 회원가입 화면에서 '회원가입'버튼 클릭 시
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    name_receive = request.form['name_give']
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    mail_receive = request.form['name_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    db.user_bmi.insert_one({'name': name_receive, 'id': id_receive, 'pw': pw_hash, 'mail': mail_receive})
+
+    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
